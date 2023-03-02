d21dd9c3dba1a5d44002c324f30e9d573774a4bc
Shaffay Bajwa <shaffay786@gmail.com>
2023-03-02 15:55:59 +0400

    update

---
+++ products.js
@@ -0,0 +1,56 @@
+let products = [
+    {
+        id:1001,
+        title:"Cat Food , 25lb Bag",
+        description:"lorem lorem lorem <em>lorem</em> lorem lorem lorem",
+        price: 2000,
+        image: "images/Arena.jpeg",
+        availableInventory:10,
+        rating:2
+    },
+    {
+        id:1002,
+        title:"Dog Food , 25lb Bag",
+        description:"lorem lorem lorem <em>lorem</em> lorem lorem lorem",
+        price: 2000,
+        image: "images/Arena.jpeg",
+        availableInventory:10,
+        rating:2
+    },
+    {
+        id:1003,
+        title:"Cat Food , 25lb Bag",
+        description:"lorem lorem lorem <em>lorem</em> lorem lorem lorem",
+        price: 2000,
+        image: "images/Arena.jpeg",
+        availableInventory:10,
+        rating:2
+    },
+    {
+        id:1004,
+        title:"Cat Food , 25lb Bag",
+        description:"lorem lorem lorem <em>lorem</em> lorem lorem lorem",
+        price: 2000,
+        image: "images/Arena.jpeg",
+        availableInventory:10,
+        rating:2
+    },
+    {
+        id:1005,
+        title:"Cat Food , 25lb Bag",
+        description:"lorem lorem lorem <em>lorem</em> lorem lorem lorem",
+        price: 2000,
+        image: "images/Arena.jpeg",
+        availableInventory:10,
+        rating:2
+    },
+    {
+        id:1006,
+        title:"Cat Food , 25lb Bag",
+        description:"lorem lorem lorem <em>lorem</em> lorem lorem lorem",
+        price: 2000,
+        image: "images/Arena.jpeg",
+        availableInventory:10,
+        rating:2
+    },
+]
